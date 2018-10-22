64dbe9d7620ac8e0e67370db580a3057e5644111
scotidodson <scotidodson@gmail.com>
2018-10-22 13:38:05 -0400

    testing out our environment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :user_shows
   has_many :shows, through: :user_shows
 
+  # testing
+  
   def show_name
     "#{self.first_name} #{self.last_name[0]}."
   end
