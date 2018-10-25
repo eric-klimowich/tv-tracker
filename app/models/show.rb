07efcbd83044180538d8b2d97f410e0e55a49a54
scotidodson <scotidodson@gmail.com>
2018-10-25 00:41:17 -0400

    added functionality for average rating to show in ABOUT info for shows

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,9 @@ class Show < ApplicationRecord
   has_many :platforms, through: :show_platforms
 
   def self.search(search)
-    if search
+    if search == "five_stars"
+      "popular"
+    elsif search
       if Show.where("title like ?", "%#{search}%")
       Show.where("title like ?", "%#{search}%")
       else
@@ -16,6 +18,29 @@ class Show < ApplicationRecord
     else
       order('id DESC')
     end
+  end
+
+  # def popular_shows
+  #   @popular_shows = []
+  #   all_ids = []
+  #   shows = UserShow.all.map { |s| s.show}.each { |s| all_ids << s.id }.sort
+  #   UserShow.where("show_id")
+  #
+  #   Model.select(:field)
+  #
+  #   from users group by last_name having max(count(*));
+  #
+  # end
+
+  def average_rating
+    reviews = self.reviews
+    sum = 0
+    total = 0
+    reviews.each do |review|
+      sum += review.rating.to_f
+      total += 1
+    end
+    @average_rating = (sum/total).round(1)
   end
 
 end
